4d0536cb2ed096de548b9b8fc38f0d4e7e771af3
devjoysarker <joysarker2927@gmail.com>
2022-03-30 20:21:25 +0600

    upload class-9

---
+++ app.js
@@ -0,0 +1,109 @@
+
+
+// let family = [
+//     {
+//         id              : 1,
+//         Name            : 'Joy Sarker',
+//         skill           : 'MERN',
+//         Location        : 'Gulshan',
+//         fee             : 500,
+//         Gender          : 'Male',
+//         age             : 30,   
+                
+//     },
+//     {
+//         id              : 2,
+//         Name            : 'Bijoy Shill',
+//         skill           : 'MERN',
+//         Location        : 'Gulshan',
+//         fee             : 500,
+//         Gender          : 'Male',
+//         age             :34,
+               
+//     },
+//     {
+//         id              : 3,
+//         Name            : 'Abbus vaai',
+//         skill           : 'Wordpress',
+//         Location        : 'Mirpur',
+//         fee             : 300,
+//         Gender          : 'Male',
+//         age             :23,
+               
+//     },
+//     {
+//         id              : 4,
+//         Name            : 'Malak akanda',
+//         skill           : 'Wordpress',
+//         Location        : 'Mirpur',
+//         fee             : 350,
+//         Gender          : 'Male',
+//         age             :26,
+               
+//     },
+//     {
+//         id              : 5,
+//         Name            : 'Tohid vai',
+//         skill           : 'PHP',
+//         Location        : 'Banani',
+//         fee             : 600,
+//         Gender          : 'Male',
+//         age             :18,
+               
+//     },
+//     {
+//         id              : 6,
+//         Name            : 'Toffazol hossin',
+//         skill           : 'PHP',
+//         Location        : 'Banani',
+//         fee             : 200,
+//         Gender          : 'Male',
+//         age             : 23,
+               
+//     },
+//     {
+//         id              : 7,
+//         Name            : 'Asok Sarker',
+//         skill           : 'Payton',
+//         Location        : 'Gazipur',
+//         fee             : 500,
+//         Gender          : 'Male',
+//         age             : 22,
+               
+//     },
+//     {
+//         id              : 8,
+//         Name            : 'Nobi hossin',
+//         skill           : 'Payton',
+//         Location        : 'Gazipur',
+//         fee             : 800,
+//         Gender          : 'Male',
+//         age             : 29,
+               
+//     },
+//     {
+//         id              : 9,
+//         Name            : 'Jagoron Roy',
+//         skill           : 'Golang',
+//         Location        : 'Mhammadpur',
+//         fee             : 400,
+//         Gender          : 'Male',
+//         age             : 23,
+               
+//     },
+//     {
+//         id              : 10,
+//         Name            : 'Jannatul-fardous',
+//         skill           : 'Golang',
+//         Location        : 'Mhammadpur',
+//         fee             : 900,
+//         Gender          : 'Female',
+//         age             : 23,
+               
+//     },
+
+    
+// ];
+
+// // let fata = JSON.stringify(family);
+// // console.log(fata);
